5a7fed25614e235327871b8544a32ef777083648
Albert Muhatia <albertmuhatia@gmail.com>
2022-11-29 12:01:22 +0300

    Update

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -55,7 +55,7 @@
                                     <div class="swiper-slide">
                                         <div class="swiper_contbox">
                                             <div class="swipper_conntent">
-                                                <img src="{{asset('uploads/properties/rosehill-3.jpg')}}" class="img-fluid " alt="images" />
+                                                <img src="{{asset('uploads/properties/rosehill-1.jpg')}}" class="img-fluid " alt="images" />
                                             </div>
                                         </div>
                                     </div>
@@ -69,7 +69,7 @@
                                     <div class="swiper-slide">
                                         <div class="swiper_contbox">
                                             <div class="swipper_conntent">
-                                                <img src="{{asset('uploads/properties/rosehill-1.jpg')}}" class="img-fluid" alt="images" />
+                                                <img src="{{asset('uploads/properties/rosehill-3.jpg')}}" class="img-fluid" alt="images" />
                                             </div>
                                         </div>
                                     </div>
@@ -282,54 +282,52 @@
             <h2>Services</h2>
         </div>
         <div class="row">
-            <div class="col-lg-3 col-md-12 m-top-0 m-bottom-40" data-aos="fade-up" data-aos-delay="150">
-                <div class="service bg-light-2 border-1 border-light box-shadow-1 box-shadow-2-hover">
+            <div  class="col-lg-4 col-md-12 m-top-0 m-bottom-40" data-aos="fade-up" data-aos-delay="150">
+                <div style="min-height:300px !important;" class="service bg-light-2 border-1 border-light box-shadow-1 box-shadow-2-hover">
                     <div class="media">
                         <i class="fa fa-home bg-base text-white rounded-100 box-shadow-1 p-top-5 p-bottom-5 p-right-5 p-left-5"></i>
                     </div>
                     <div class="agent-section p-top-35 p-bottom-30 p-right-25 p-left-25">
-                        <h4 class="m-bottom-15 text-bold-700">Houses</h4>
-                        <p>Kenya is a country in Africa with coastline on the Indian Ocean. It encompasses savannah, lakelands, the dramatic Great Rift Valley and mountain highlands. It's also home to wildlife like lions, elephants and rhino</p>
+                        <h4 class="m-bottom-15 text-bold-700">Buying</h4>
+                        <p>
+                            We handpick high-end luxurious homes across the globe.
+
+                            With our distinctive taste of class we list only the best homes.
+
+                            With access to unrivaled property for sale Globally, premium Homes is expertly positioned to help you in your property search.
+                        </p>
                         <a class="text-base text-base-dark-hover text-size-13" href="properties-full-list#">Read More <i class="fa fa-long-arrow-right ml-2"></i></a>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-12 m-top-40 m-bottom-40" data-aos="fade-up" data-aos-delay="250">
-                <div class="service bg-light-2 border-1 border-light box-shadow-1 box-shadow-2-hover">
+            <div class="col-lg-4 col-md-12 m-top-40 m-bottom-40" data-aos="fade-up" data-aos-delay="250">
+                <div style="min-height:300px" class="service bg-light-2 border-1 border-light box-shadow-1 box-shadow-2-hover">
                     <div class="media">
                         <i class="fas fa-building bg-base text-white rounded-100 box-shadow-1 p-top-5 p-bottom-5 p-right-5 p-left-5"></i>
                     </div>
                     <div class="agent-section p-top-35 p-bottom-30 p-right-25 p-left-25">
-                        <h4 class="m-bottom-15 text-bold-700">Residence</h4>
-                        <p>Kenya is a country in Africa with coastline on the Indian Ocean. It encompasses savannah, lakelands, the dramatic Great Rift Valley and mountain highlands. It's also home to wildlife like lions, elephants and rhino</p>
+                        <h4 class="m-bottom-15 text-bold-700">Renting</h4>
+                        <p>
+                            Welcome to Premium Homes  - your starting point to the finest property to rent across the world and gateway to luxury global rental properties through Premium Homes.
+Premium Homes offers local real estate agents in major cities offering a wide range of property for rent in Listed areas including apartments, town houses, maisonettes and houses.
+                        </p>
                         <a class="text-base text-base-dark-hover text-size-13" href="properties-full-list#">Read More <i class="fa fa-long-arrow-right ml-2"></i></a>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-12 m-top-40 m-bottom-40 commercial" data-aos="fade-up" data-aos-delay="350">
-                <div class="service bg-light-2 border-1 border-light box-shadow-1 box-shadow-2-hover">
+            <div class="col-lg-4 col-md-12 m-top-40 m-bottom-40 commercial" data-aos="fade-up" data-aos-delay="350">
+                <div style="min-height:300px" class="service bg-light-2 border-1 border-light box-shadow-1 box-shadow-2-hover">
                     <div class="media">
                         <i class="fas fa-warehouse bg-base text-white rounded-100 box-shadow-1 p-top-5 p-bottom-5 p-right-5 p-left-5"></i>
                     </div>
                     <div class="agent-section p-top-35 p-bottom-30 p-right-25 p-left-25">
-                        <h4 class="m-bottom-15 text-bold-700">Commercial</h4>
+                        <h4 class="m-bottom-15 text-bold-700">Property Advisory</h4>
                         <p>Kenya is a country in Africa with coastline on the Indian Ocean. It encompasses savannah, lakelands, the dramatic Great Rift Valley and mountain highlands. It's also home to wildlife like lions, elephants and rhino</p>
                         <a class="text-base text-base-dark-hover text-size-13" href="properties-full-list#">Read More <i class="fa fa-long-arrow-right ml-2"></i></a>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-12 m-top-40 m-bottom-40 commercial" data-aos="fade-up" data-aos-delay="450">
-                <div class="service bg-light-2 border-1 border-light box-shadow-1 box-shadow-2-hover">
-                    <div class="media">
-                        <i class="fas fa-map-marker bg-base text-white rounded-100 box-shadow-1 p-top-5 p-bottom-5 p-right-5 p-left-5"></i>
-                    </div>
-                    <div class="agent-section p-top-35 p-bottom-30 p-right-25 p-left-25">
-                        <h4 class="m-bottom-15 text-bold-700">Plots</h4>
-                        <p>Kenya is a country in Africa with coastline on the Indian Ocean. It encompasses savannah, lakelands, the dramatic Great Rift Valley and mountain highlands. It's also home to wildlife like lions, elephants and rhino</p>
-                        <a class="text-base text-base-dark-hover text-size-13" href="properties-full-list#">Read More <i class="fa fa-long-arrow-right ml-2"></i></a>
-                    </div>
-                </div>
-            </div>
+
         </div>
     </div>
 </section>
