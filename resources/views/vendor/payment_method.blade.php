e9ea26b5fe95983e4282a3e8ff56a9b7b93e8394
Albert Muhatia <albertmuhatia@gmail.com>
2022-12-05 11:34:06 +0300

    Update

--- resources/views/vendor/payment_method.blade.php
+++ resources/views/vendor/payment_method.blade.php
@@ -209,10 +209,10 @@
                                              <div class="payment-card">
                                                  <header class="payment-card-header cursor-pointer" data-toggle="collapse" data-target="#paypal" aria-expanded="true">
                                                      <div class="payment-card-title flexbox">
-                                                         <h4>PayPal</h4>
+                                                         <h4>Card Payment</h4>
                                                      </div>
                                                      <div class="pull-right">
-                                                         <img src="{{asset('theme/images/paypal.png')}}" class="img-responsive" alt="">
+                                                         <img src="{{asset('uploads/logo.png')}}" class="img-responsive" alt="">
                                                      </div>
                                                  </header>
                                                  <div class="collapse" id="paypal" role="tablist" aria-expanded="false">
@@ -246,38 +246,25 @@
                                              <div class="payment-card mb-0">
                                                  <header class="payment-card-header cursor-pointer" data-toggle="collapse" data-target="#debit-credit" aria-expanded="true">
                                                      <div class="payment-card-title flexbox">
-                                                         <h4>Credit / Debit Card</h4>
+                                                         <h4>M-Pesa Express</h4>
                                                      </div>
                                                      <div class="pull-right">
-                                                         <img src="{{asset('theme/images/credit.png')}}" class="img-responsive" alt="">
+                                                         <img src="{{asset('uploads/1200px-M-PESA_LOGO-01.svg.png')}}" class="img-responsive" alt="">
                                                      </div>
                                                  </header>
                                                  <div class="collapse" id="debit-credit" role="tablist" aria-expanded="false">
                                                      <div class="payment-card-body">
                                                          <div class="row mrg-bot-20">
                                                              <div class="col-sm-6">
-                                                                 <label>Card Holder Name</label>
-                                                                 <input type="text" class="form-control" placeholder="Chris Seail">
+                                                                 <label>Your Name</label>
+                                                                 <input type="text" value="{{Auth::user()->name}}" class="form-control" placeholder="Chris Seail">
                                                              </div>
                                                              <div class="col-sm-6">
-                                                                 <label>Card No.</label>
-                                                                 <input type="email" class="form-control" placeholder="1800 5785 6758 2458">
+                                                                 <label>M_Pesa Number</label>
+                                                                 <input type="email" value="{{Auth::user()->mobile}}" class="form-control" placeholder="1800 5785 6758 2458">
                                                              </div>
                                                          </div>
-                                                         <div class="row mrg-bot-20">
-                                                             <div class="col-sm-4 col-md-4">
-                                                                 <label>Expire Month</label>
-                                                                 <input type="text" class="form-control" placeholder="09">
-                                                             </div>
-                                                             <div class="col-sm-4 col-md-4">
-                                                                 <label>Expire Year</label>
-                                                                 <input type="email" class="form-control" placeholder="2022">
-                                                             </div>
-                                                             <div class="col-sm-4 col-md-4">
-                                                                 <label>CCV Code</label>
-                                                                 <input type="email" class="form-control" placeholder="258">
-                                                             </div>
-                                                         </div>
+
                                                          <div class="row mrg-bot-20">
                                                              <div class="col-sm-7">
                                                                  <span class="custom-checkbox d-block font-12 mb-2">
@@ -289,15 +276,15 @@
                                                              </div>
                                                              <div class="col-sm-5 padd-top-10 text-right">
                                                                  <label>Total Order</label>
-                                                                 <h2 class="mrg-0"><span class="theme-cl">$</span>987</h2>
+                                                                 <h2 class="mrg-0"><span class="theme-cl">kes</span>12540</h2>
                                                              </div>
                                                              <div class="col-sm-12 bt-1 padd-top-15 pt-3">
                                                                  <span class="custom-checkbox d-block font-12 mb-3">
                                                                  <input type="checkbox" id="privacy1">
                                                                  <label for="privacy1"></label>
-                                                                 By ordering you are agreeing to our <a href="#" class="theme-cl">Privacy policy</a>.
+                                                                 By ordering you are agreeing to our <a href="{{url('/')}}/privacy-policy" class="theme-cl">Privacy policy</a>.
                                                                  </span>
-                                                                 <button type="submit" class="btn btn-m btn-success">Checkout</button>
+                                                                 <button type="submit" class="btn btn-m btn-success">Pay Now</button>
                                                              </div>
                                                          </div>
                                                      </div>
@@ -307,24 +294,7 @@
                                      </div>
                                  </div>
                              </div>
-                             <div class="col-md-12">
-                                 <div class="tr-single-box mb-0">
-                                     <div class="tr-single-body">
-                                         <div class="tr-single-header">
-                                             <h4><i class="fa fa-star-o"></i>Booking Summary</h4>
-                                         </div>
-                                         <div class="booking-price-detail side-list no-border mb-3">
-                                             <h5>Reservation Details</h5>
-                                             <ul>
-                                                 <li>Date<strong class="pull-right">18 Jun 2018</strong></li>
-                                                 <li>Time<strong class="pull-right">9pm 10pm</strong></li>
-                                                 <li>From<strong class="pull-right">10 jan 2019</strong></li>
-                                             </ul>
-                                         </div>
 
-                                     </div>
-                                 </div>
-                             </div>
                          </section>
                          <!-- END SECTION PAYMENT-METHOD -->
                      </div>
