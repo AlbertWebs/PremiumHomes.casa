35383a69037c077fd10d6b1f8fd400113048d41e
Albert Muhatia <albertmuhatia@gmail.com>
2022-12-09 18:40:54 +0300

    .

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -9,23 +9,7 @@
       <meta name="keywords" content="Invce - Invoice HTML Template">
       <meta name="robots" content="INDEX,FOLLOW">
       <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-      <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicons/apple-icon-57x57.png">
-      <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicons/apple-icon-60x60.png">
-      <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicons/apple-icon-72x72.png">
-      <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicons/apple-icon-76x76.png">
-      <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicons/apple-icon-114x114.png">
-      <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/apple-icon-120x120.png">
-      <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicons/apple-icon-144x144.png">
-      <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/apple-icon-152x152.png">
-      <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-icon-180x180.png">
-      <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicons/android-icon-192x192.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicons/favicon-96x96.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
-      <link rel="manifest" href="assets/img/favicons/manifest.json">
-      <meta name="msapplication-TileColor" content="#ffffff">
-      <meta name="msapplication-TileImage" content="assets/img/favicons/ms-icon-144x144.png">
-      <meta name="theme-color" content="#ffffff">
+
       <link rel="preconnect" href="https://fonts.googleapis.com/">
       <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
@@ -51,7 +35,7 @@
                            <div class="row align-items-center justify-content-between">
                               <div class="col-auto">
                                  <div class="header-bottom_left">
-                                    <p><b>Guest Name : </b>Amit Harnandes</p>
+                                    <p><b>Client Name : </b>Amit Harnandes</p>
                                     <div class="shape"></div>
                                     <div class="shape"></div>
                                  </div>
@@ -75,8 +59,8 @@
                         </div>
                         <div class="col-auto">
                            <div class="booking-info">
-                              <p><b>Check In: </b>24/07/2022</p>
-                              <p><b>Check Out: </b>27/07/2022</p>
+                              <p><b>Created Date: </b>24/07/2022</p>
+                              <p><b>Due Date: </b>27/07/2022</p>
                            </div>
                         </div>
                         <div class="col-auto">
