68cdd72b17dacaec16a621b6e98cbe5dfbc4fd55
Albert Muhatia <albertmuhatia@gmail.com>
2022-12-16 15:54:01 +0300

    Update

--- app/Http/Controllers/MPESAController.php
+++ app/Http/Controllers/MPESAController.php
@@ -31,7 +31,7 @@ class MPESAController extends Controller
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Basic ".$credentials));
         curl_setopt($curl, CURLOPT_HEADER,false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
         $curl_response = curl_exec($curl);
