5b8ed273390d358c5700c428d6819a4c7dd0aeab
Albert Muhatia <albertmuhatia@gmail.com>
2022-12-17 13:37:42 +0300

    Update

--- app/Http/Controllers/MPESAController.php
+++ app/Http/Controllers/MPESAController.php
@@ -28,7 +28,7 @@ class MPESAController extends Controller
         $url = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$credentials.''));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Basic $credentials"));
         curl_setopt($curl, CURLOPT_HEADER,false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
