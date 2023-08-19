eb713c8d6255e4cd2dbe508f22344fa123d2c871
Albert Muhatia <albertmuhatia@gmail.com>
2023-08-19 14:21:09 +0300

    Update Type Title

--- resources/views/front/properties.blade.php
+++ resources/views/front/properties.blade.php
@@ -506,7 +506,7 @@
                 <div class="row">
                     <div class="section-title col-md-5 pl-44">
                         <h3>Properties</h3>
-                        <h2>For Sale</h2>
+                        <h2 style="text-transform: capitalize">For {{$id}}</h2>
                     </div>
                 </div>
                 <div class="row portfolio-items">
